8228a88ecca95bfc9aaad81c9cb397190f934801
Grace Yin <graceyin1218@gmail.com>
2018-05-11 16:42:17 -0400

    hopefully the last one...

--- imports_test.go
+++ imports_test.go
@@ -721,12 +721,13 @@ import (
 type I int
 `)
 	// The AddImport order here matters.
-	AddImport(fset, file, "golang.org/x/tools/go/ast/astutil")
+	//AddImport(fset, file, "golang.org/x/tools/go/ast/astutil")
+	AddImport(fset, file, "github.com/DieracDelta/astutil")
 	AddImport(fset, file, "os")
 	want := `package main
 
 import (
-	"golang.org/x/tools/go/ast/astutil"
+  "github.com/DieracDelta/astutil"
 	"os"
 )
 
